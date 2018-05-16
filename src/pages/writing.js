9d3a0476825d48e24b067920ee262e82057865d5
Rebecca Weir <rebeccaweir12@gmail.com>
2018-05-16 18:43:16 -0400

    Add basic writing styling

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import NavBar from '../components/navBar';
+import Footer from '../components/footer';
 
 const WritingPage = () => (
-  <div>
+  <div id="writing">
     <NavBar background="light" hamburger="dark"/>
+    <Footer />
+    <h1>You've found a page that doesn't exist yet!</h1>
+    <h3>Info on my writing to come soon!</h3>
   </div>
 );
 
